1e71c3c600adb9cc5e822057bfc085fd2fb70c0a
CraigB73 <craigbritton73@gmail.com>
2024-06-25 21:43:12 +0200

    Transaction html inprogress

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import transaction_view
+from . import views
 
 urlpatterns = [
     path('', transaction_view, name='transaction'),
+     path('transaction/<pk>/delete/', views.delete_transaction, name='delete_transaction'),
 ]
--- transaction/views.py
+++ transaction/views.py
@@ -1,53 +1,57 @@
-from django.shortcuts import render, redirect, get_list_or_404
-from .models import Transaction, Budget
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from .models import Transaction, Budget
 from .forms import TransactionForm
 
-
 # Create your views here.
-def transaction_view(request):
+@login_required
+def transaction_view(request):   
+    #Gets the allowance created in the Budget model for authorized user
     try:
         budget = Budget.objects.get(user=request.user)
     except Budget.DoesNotExist:
-        return render(request, 'transaction/transactions.html')
+        return render(request, 'transaction/transaction.html')
     allowance = budget.allowance
     budget.save()
-    
-    if request.method == "POST":
+
+    if request.method == 'POST':
         form = TransactionForm(request.POST)
-        transaction = form.save(Commit=False)
-        transaction.save()
-        return redirect('transaction')
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('transaction')
     
-    else: 
+    else:
         form = TransactionForm()
-        
-    transactions = Transaction.objects.filter(user = request.user).order_by("_created_on")
-    # Provides the purnchase transaction use to subtract from users allowance
-    total_transactions = sum(transaction.transaction_amount for transaction in transactions)
     
-    # Calculates remaining allowance 
-    alllowance_remaining = allowance - total_transactions
+    transactions = Transaction.objects.filter(user = request.user).order_by("-created_on")
+    total_transactions = sum(transaction.transaction_amount for transaction in transactions )
     
-    """
-    Creates a list to be render in transaction template
-    """
+    allowance_remaining = allowance - total_transactions
+    
     list_transactions = []
     for transaction in transactions:
         list_transactions.append({
             "transaction": transaction,
             "amount": transaction.transaction_amount,
             "location": transaction.location,
-            "category": transaction.category,
+            "category": transaction.category
         })
-    
-    return render(request, "transaction/transaction.html", {
-        "form": form,
-        "allowance": allowance,
-        "transactions": transactions,
-        "total_transactions": total_transactions,
-        "remaining_allowance": alllowance_remaining,
-        "list_transactions": list_transactions,
-        "saved": True,
-    })
-    
+        
+  
+    return render(request, 'transaction/transaction.html', {
+            "form": form,
+            "allowance": allowance,
+            "transactions": transactions,
+            "total_transaction": total_transactions,
+            "remaining_allowance": allowance_remaining,
+            "list_transactions": list_transactions,
+            "saved": True
+            })
+
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    transaction.delete()
+    return redirect(reverse_lazy("transaction"))

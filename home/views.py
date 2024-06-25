ba9473f2216f2beec06edfce3dbc20d9460b22b8
CraigB73 <craigbritton73@gmail.com>
2024-06-25 15:59:32 +0200

    Add content to Home.html and css

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from allauth.account.forms import LoginForm
 
 # Create your views here.
 def home_view(request):
-  return render(request, "home/home.html")
+    """
+    Renders the Home page
+    """
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return redirect("home/home.html")
+    else:
+        form = LoginForm()
+    return render(request, "home/home.html", { "form": form})

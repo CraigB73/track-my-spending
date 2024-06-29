c599ef974c36181ddefa46881d138e6f6d7a7fab
CraigB73 <craigbritton73@gmail.com>
2024-06-29 22:26:43 +0200

    Add dj restframework, Add Doughtnut chart, update transaction view for chart

--- track_my_spending/settings.py
+++ track_my_spending/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1",
     ".herokuapp.com",]
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+     'rest_framework',
     'budget',
     'transaction',
     'home',
--- transaction/urls.py
+++ transaction/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import transaction_view, delete_transaction
-
+from.views import transaction_view, delete_transaction, get_chart_data
 
 urlpatterns = [
     path('', transaction_view, name='transaction'),
-    path('transaction/<pk>/delete/', delete_transaction, name='delete_transaction'),
-    # ...
+    path('delete/<int:pk>/', delete_transaction, name='delete_transaction'),
+    path('chart-data/', get_chart_data, name='chart_data'),
 ]

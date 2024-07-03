ad56d985140e43a233a39935bcee40dc98150ece
CraigB73 <craigbritton73@gmail.com>
2024-07-03 11:40:43 +0200

    Moved script into transaction_script.js/ Add styling to chart

---
+++ transaction/static/transaction/js/transaction_script.js
@@ -0,0 +1,59 @@
+console.log('test')  
+
+const ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Total',
+        data: [],
+        backgroundColor: [
+          '#36454f',
+          '#536878',
+          '#708090',
+          '#536872',
+          '#e3e7ee'],
+        borderWidth: 1
+      }],
+
+    },
+    options: {
+      reposive: true,
+      maintainAspectRation: true,
+      plugins: {
+        title: {
+          display: true,
+          text: "Total Transactions",
+          position: "top",
+          align: 'start',
+          color: "#e3e7ee",
+          font: {
+            size: 26
+          }
+        },
+        legend: {
+          position: "right",
+          align: "",
+          labels: {
+            color: "#e3e7ee",
+            font: {
+              size: 12,
+              weigth: "bold"
+            }
+          }
+        }
+      }
+    }
+  });
+
+  async function updateChart() {
+    const response = await fetch('transaction/chart-data/');
+    const data = await response.json();
+
+    myChart.data.labels = data.labels;
+    myChart.data.datasets[0].data = data.data;
+    myChart.update();
+  }
+
+  document.addEventListener('DOMContentLoaded', updateChart);

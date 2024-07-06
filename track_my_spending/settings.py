225e1136babf50c6bfb1af6defd09465cabbbe98
CraigB73 <craigbritton73@gmail.com>
2024-07-06 23:42:53 +0200

    Fix style in About and added 2 chart images

--- track_my_spending/settings.py
+++ track_my_spending/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'budget/static/budget'),
